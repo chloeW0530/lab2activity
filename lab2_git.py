ff5cb6a8cd4f8fc243caca9b4b3450eb1d294da6
Chloe Wang <100745270+koko0530@users.noreply.github.com>
2022-08-01 16:46:26 +1200

    Update lab2_git.py

--- lab2_git.py
+++ lab2_git.py
@@ -82,7 +82,11 @@ def toss_coin():
     """TODO: Add your code here
         Hint: Using random.randint() function.
     """
-    coin = 'Head'
+    rand_num = random.randint(1,2)
+    if rand_num == 1:
+        coin = 'Head'
+    else:
+        coin = 'Tail'
     return coin
 
 
